b9c5a5b1a90ecb71e2c8d26e2e731c2094558ffa
cftest120241104-lang <cftest120241104-lang@users.noreply.github.com>
2026-04-04 09:34:01 +0800

    猪消消消 H5 单机版

---
+++ src/settings.4b547.js
@@ -0,0 +1,27 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/Main.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "cb4f5",
+        audio: "adc88",
+        game: "4d18e",
+        game2: "1c4f1",
+        resources: "2e4a7",
+        main: "aa486"
+    }
+};
