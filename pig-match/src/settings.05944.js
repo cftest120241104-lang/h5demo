a53323779719a5d89855012755fa92e53e86603d
cftest120241104-lang <cftest1.20241104@gmail.com>
2026-04-22 09:54:06 +0800

    deploy: 更新 pig-match 换皮版 + vConsole
    修复内容：tween 回调竞态、scanOneTile 重复 case、VictoryView delay 属性

---
+++ pig-match/src/settings.05944.js
@@ -0,0 +1,27 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/Main.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "cb4f5",
+        audio: "fb4c6",
+        game: "b2ba5",
+        game2: "e0a95",
+        resources: "9fe06",
+        main: "0a50b"
+    }
+};
